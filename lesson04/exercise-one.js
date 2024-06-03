301ab06d992cdcc2bee9ac3f353181ececd6c476
Jack T <tindalj@outlook.com>
2024-06-02 22:07:38 -0700

    lesson4 challenge1

---
+++ lesson04/exercise-one.js
@@ -0,0 +1,15 @@
+let fiveStrings = ['one', 'two', 'three', 'four', 'five'];
+
+console.log(fiveStrings);
+
+fiveStrings.push('six');
+
+console.log(fiveStrings);
+
+fiveStrings.splice(2, 1);
+
+console.log(fiveStrings);
+
+const list = fiveStrings.join(", ");
+
+console.log(list);
